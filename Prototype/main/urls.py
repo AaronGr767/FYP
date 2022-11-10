a572d89ea1ced1596e5a27bd06fbd3469a9af93d
AaronGr767 <aarongroarke2@gmail.com>
2022-11-10 17:15:50 +0000

    Updated urls to allow for register/login

---
+++ Prototype/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.views.generic.base import TemplateView
+
+app_name = "main"
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+]
