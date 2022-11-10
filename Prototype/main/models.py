e60ec69bbcb93f2a732bc456843dd9bf7784da81
AaronGr767 <aarongroarke2@gmail.com>
2022-11-10 20:32:48 +0000

    Added model for user profile and added it to admin

--- Prototype/main/models.py
+++ Prototype/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+# Register your models here.
 
+User = get_user_model()
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150, primary_key=True)
+    test = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.user}"
 # Create your models here.
