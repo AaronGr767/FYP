1a52c833fe66fe5fd70b1e689a4a8ed306f5369d
AaronGr767 <aarongroarke2@gmail.com>
2022-11-21 15:53:34 +0000

    Created necessary pages

--- Prototype/main/urls.py
+++ Prototype/main/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
     path('registration', views.register_request, name="registration"),
     path('home', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('create', TemplateView.as_view(template_name="createTrip.html"), name="createTrip"),
+    path('manage', TemplateView.as_view(template_name="manageTrip.html"), name="manageTrip"),
+    path('edit', TemplateView.as_view(template_name="editProfile.html"), name="editProfile"),
 ]
