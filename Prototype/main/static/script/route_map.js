825e8c227942bc8797ee54cc22f1f8f0c427e0cb
AaronGr767 <aarongroarke2@gmail.com>
2022-12-07 04:43:00 +0000

    javascript which handles majority of mapping logic

---
+++ Prototype/main/static/script/route_map.js
@@ -0,0 +1,42 @@
+
+$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + api_key + "&libraries=places")
+.done(function( script, textStatus ) {
+    google.maps.event.addDomListener(window, "load", initMap)
+
+})
+
+const waypts = [
+        {location: {lat: lat_1, lng: long_1},
+        stopover: true}
+        ];
+
+function initMap() {
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var map = new google.maps.Map(document.getElementById('map-route'));
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    // const selectedMode = document.getElementById("mode").value;
+    var selectedMode=localStorage.getItem('transMode');
+
+    directionsService.route({
+        origin: origin,
+        destination: destination,
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: google.maps.TravelMode[selectedMode],
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+
+
+      } else {
+
+        alert('Directions request failed due to ' + status);
+      }
+    });
+}
---
+++ Prototype/main/static/script/set_location.js
@@ -0,0 +1,46 @@
+
+$.getScript( "https://maps.googleapis.com/maps/api/js?key=" + api_key + "&libraries=places")
+.done(function( script, textStatus ) {
+    google.maps.event.addDomListener(window, "load", initAutocomplete())
+
+})
+
+function initAutocomplete() {
+
+    window['autocomplete_address'] = new google.maps.places.Autocomplete(
+      document.getElementById('starting_address'),
+    {
+       types: ['address'],
+       componentRestrictions: {'country': ['ie']},
+    })
+
+}
+
+
+function beginTrip (addy){
+
+    // const selectedMode = document.getElementById("mode").value;
+    localStorage.setItem('transMode',document.getElementById("mode").value);
+
+    var geocoder = new google.maps.Geocoder()
+    var location = document.getElementById('starting_address').value
+
+    geocoder.geocode( { 'address': location}, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+
+            console.log('lat_a=' + latitude + '&long_a=' + longitude)
+            var queryCoords = 'lat_a=' + latitude + '&long_a=' + longitude
+
+        locString = '/route?' + queryCoords
+        window.location.assign(locString)
+        }
+    });
+}
+
+
+
+
+
