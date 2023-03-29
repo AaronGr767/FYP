ba53eb6d6c810722f4888a15b478ac6c013d2758
AaronGr767 <aarongroarke2@gmail.com>
2023-03-29 12:04:37 +0100

    Comment & format set_location.js

--- Prototype/main/static/script/set_location.js
+++ Prototype/main/static/script/set_location.js
@@ -1,5 +1,6 @@
 let myLoc = false;
 
+// Autocompletes user inputted address with data from Google Maps
 function initAutocomplete() {
 
     let input = document.getElementById('starting_address');
@@ -11,7 +12,9 @@ function initAutocomplete() {
 
     let autocomplete = new google.maps.places.Autocomplete(input, options);
 
+    // Listens for and retrieves the users selected location before storing it in local storage
     autocomplete.addListener('place_changed', function () {
+        myLoc = false;
         let place = autocomplete.getPlace();
         let chosenAdd = new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng());
         localStorage.setItem('startLoc', JSON.stringify(chosenAdd));
@@ -19,10 +22,11 @@ function initAutocomplete() {
 
 }
 
-
+// Checks to see if the user has chosen their current address or inputted a chosen address and proceeds accordingly
 function checkType() {
     let manualEntry = document.getElementById('starting_address').value
 
+    // If the user has not picked any address, sets their location value to 'None'
     if (myLoc == false && manualEntry == "") {
         console.log("Path to next page will go here when mapping is sorted")
         localStorage.setItem('startLoc', JSON.stringify("None"));
@@ -31,7 +35,9 @@ function checkType() {
 }
 
 
+// Retrieves the users current location if they choose so and stores it in local storage
 function getLocation() {
+    // If geolocation is supported by the browser
     if (navigator.geolocation) {
         myLoc = true
         navigator.geolocation.getCurrentPosition(function (position) {
